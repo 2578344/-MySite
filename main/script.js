5867a6cd52d2c8a866c5147c6afdc25770493f78
Michal Golabek <2578344g@student.gla.ac.uk>
2023-02-24 18:26:04 +0000

    added email-form to contact me

---
+++ main/script.js
@@ -0,0 +1,18 @@
+var check = document.getElementById("first").classList.add('vanish');
+var isformOpen = false;
+var pressed = 0;
+function change(){
+    document.getElementById("first").classList.remove('vanish');
+    document.getElementById("second").innerHTML = "Send";
+    isformOpen = true;
+    pressed += 1;
+    if (isformOpen == true && pressed % 2 ==0){
+        document.getElementById("email-form").submit();
+        isformOpen = false;
+    }
+}
+
+function exit() {
+    document.getElementById("first").classList.add('vanish');
+    document.getElementById("second").innerHTML = "Contact Me";
+}
